68510c9b6b617bf37faddfacb94656e2d1836460
beyondmur <beyondmur@ukr.net>
2020-12-02 21:27:45 +0200

    кек

---
+++ script.js
@@ -0,0 +1,18 @@
+var modal = document.getElementById("modal");
+var button = document.getElementById("button_window");
+var close = document.getElementById("close")[0];
+
+button.onclick = function () {
+    modal.style.display = "block";
+};
+
+close.onclick = function () {
+    modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+    if(event.target === modal){
+        modal.style.display = "none";
+    }
+
+};
